0fc18a5016bdd7591d9004d50d450740a9084f9b
Olga Karpenko <okarpenko@usfca.edu>
2024-03-19 09:39:16 -0700

    Updated the exercise.

--- src/main/java/binarySearchTrees/exercise03_19/BinarySearchTree.java
+++ src/main/java/binarySearchTrees/exercise03_19/BinarySearchTree.java
@@ -1,8 +1,5 @@
 package binarySearchTrees.exercise03_19;
 
-/** Implementation of a basic binary search tree.
- *  The code has been modified from the code of Prof. Galles.
- */
 class BinarySearchTree {
 
     /** An inner class representing a node in a BST tree */
@@ -22,63 +19,16 @@ class BinarySearchTree {
         root = null; // initially, the tree is empty
     }
 
-    /**
-     * Insert a given element into the BST tree
-     * @param elem element to insert into the BST tree
-     */
-    public void insert(int elem) {
-        root = insert(root, elem);
-    }
-
-    /**
-     * Return true if a given element is in the tree
-     * @param elem element to find
-     * @return true if the element is present, false otherwise
-     */
-    public boolean find(int elem) {
-        return find(root, elem);
-    }
-
-    /**
-     * Delete the node with the given element from the tree
-     * @param elem element to remove
-     */
-    public void delete(int elem) {
-        root = delete(root, elem);
-    }
-
     /** Print a binary tree (preorder) */
     public void printPreorder() {
         printPreorder(root);
     }
 
-    // private helper methods ---------------------------
-    /**
-     * Find elem in the tree with the given root
-     * @param tree root of a tree
-     * @param elem element to find
-     * @return true if the node with this element is present in the tree
-     */
-    private boolean find(BSTNode tree, int elem) {
-        if (tree == null)
-            return false;
-        if (elem == tree.data)
-            return true;
-        if (elem < tree.data)
-            return find(tree.left, elem);
-        else
-            return find(tree.right, elem);
-    }
-
     public boolean findIterative(int elem) {
         BSTNode current = root;
         while (current != null) {
-            if (current.data == elem)
-                return true;
-            if (current.data < elem)
-                current = current.right;
-            else
-                current = current.left;
+            // FILL IN CODE
+
         }
         return false;
     }
@@ -92,128 +42,40 @@ class BinarySearchTree {
         }
         while (current != null) {
             if (elem >= current.data) {
-                prev = current;
-                current = current.right;
+                // FILL IN CODE: update prev and current
             }
             else {
-                prev = current;
-                current = current.left;
+                // FILL IN CODE: update prev and current
+
             }
         }
         if (elem < prev.data) {
-            prev.left = new BSTNode(elem);
+            // FILL IN CODE: create a new node and attach to prev
         }
         else {
-            prev.right = new BSTNode(elem);
-        }
-
-        // FILL IN CODE
-
-    }
-
-    /**
-     * Insert elem into the tree with the given root
-     * @param tree root of a tree
-     * @param elem element to insert
-     * @return the root of a tree that contains the new node
-     */
-    private BSTNode insert(BSTNode tree, int elem) {
-        if (tree == null) {
-            return new BSTNode(elem);
-        }
-        if (elem < tree.data) {
-            tree.left = insert(tree.left, elem);
-            return tree;
-        } else {
-            tree.right = insert(tree.right, elem);
-            return tree;
+            // FILL IN CODE: create a new node and attach to prev
         }
     }
 
-    /**
-     * Find the node with the value of elem in the tree with given
-     * root and delete it
-     * @param tree root of a tree
-     * @param elem element to delete
-     * @return the root of a tree
-     */
-    private BSTNode delete(BSTNode tree, int elem) {
-        if (tree == null)
-            return null;
-        if (tree.data == elem) { // found node with elem
-            if (tree.left == null) // has one child
-                return tree.right;
-            else if (tree.right == null) // has one child
-                return tree.left;
-            else { // has two children
-                if (tree.right.left == null) {
-                    // if the right subtree has no left tree
-                    // then the value at the root of the right subtree
-                    // is the smallest value in the right subtree.
-                    // it means we want to place the value of tree.right at tree.data
-                    // and remove tree.right
-                    tree.data = tree.right.data;
-
-                    // delete the root of the right subtree
-                    tree.right = tree.right.right;
-                    return tree;
-                } else {
-                    // need to find the node with the smallest value
-                    // in the right subtree
-                    tree.data = removeSmallest(tree.right);
-                    return tree;
-                }
-            }
-        } else if (elem < tree.data) { // find and delete in left subtree
-            tree.left = delete(tree.left, elem);
-            return tree;
-        } else {
-            tree.right = delete(tree.right, elem); // find and delete in right subtree
-            return tree;
+    private void printPreorder(BSTNode root) {
+        if (root != null) {
+            System.out.print(" " + root.data + " ");
+            printPreorder(root.left);
+            printPreorder(root.right);
         }
     }
-
-    /**
-     * Find and remove the node with the smallest value in the tree
-     * @param tree root of a tree
-     * @return the smallest element in the tree
-     */
-    private int removeSmallest(BSTNode tree) {
-        // the smallest value is always in the left most subtree
-        if (tree.left.left == null) { // the smallest value is in tree.left
-            int smallest = tree.left.data;
-            tree.left = tree.left.right; // delete it
-            return smallest;
-        } else {
-            return removeSmallest(tree.left);
-        }
-    }
-
-
-    /** Serialize a given tree using preorder traversal
-     *  using / as a sentinel character
-     *  using / as a sentinel character
-     * @param tree
-     */
-    private void printPreorder(BSTNode tree) {
-        if (tree != null) {
-            System.out.print(" " + tree.data + " ");
-            printPreorder(tree.left);
-            printPreorder(tree.right);
-        } else
-            System.out.print("/");
-    }
-
 
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
-        tree.insert(6);
-        tree.insert(1);
-        tree.insert(10);
-        tree.insert(8);
-        tree.insert(12);
-        tree.insert(16);
+        tree.insertIterative(6);
+        tree.insertIterative(1);
+        tree.insertIterative(10);
+        tree.insertIterative(8);
+        tree.insertIterative(12);
+        tree.insertIterative(16);
         tree.printPreorder();
+        System.out.println(tree.findIterative(12));
+        System.out.println(tree.findIterative(13));
         System.out.println();
         tree.printPreorder();
         System.out.println();
